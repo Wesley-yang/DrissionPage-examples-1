cc25864749fc0f8d4f39c43575874b468ef0102f
g1879 <g1879@qq.com>
2020-06-02 23:42:35 +0800

    完善

--- 猫眼电影/榜单2.py
+++ 猫眼电影/榜单2.py
@@ -14,26 +14,22 @@ class 榜单页(ListPageS):
         }
         super().__init__(drission, 首页url, **xpaths)
 
-    def check_page(self):
-        return True
-
 
 if __name__ == '__main__':
     drission = Drission()
     列表首页url = 'https://maoyan.com/board/4'
-    保存路径 = 'result'
     待爬内容 = [
-        ['//a[1]', 'href'],
-        ['//p[@class="name"]/a', 'title'],
-        # ['//p[@class="name"]/a', 'title'],
-        # ['//p[@class="name"]/a', 'title'],
-        # ['//p[@class="name"]/a', 'title'],
-        # ['//p[@class="name"]/a', 'title'],
-        # ['//p[@class="name"]/a', 'title'],
-        # ['//p[@class="name"]/a', 'title'],
+        ['//i', 'text'],  # 序号
+        ['//p[@class="name"]/a', 'title'],  # 标题
+        # ['//p[@class="name"]/a', 'title'],  # 演员
+        # ['//p[@class="name"]/a', 'title'],  # 时间
+        # ['//p[@class="name"]/a', 'title'],  # 地点
+        ['//p[@class="score"]', 'text'],  # 评分
+        ['//img', 'src'],  # 封面url
+        ['//a[1]', 'href']  # 链接
     ]
 
     page = 榜单页(drission, 列表首页url)
-    url_list = page.get_列表(待爬内容)
-    for url in url_list:
-        print(url)
+    结果列表 = page.get_列表(待爬内容)
+    for 数据 in 结果列表:
+        print(数据)
